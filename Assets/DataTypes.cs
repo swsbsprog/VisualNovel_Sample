c52730efef0b35654c5851a90304002072c90b8a
hktrollgames <hktrollgames@gmail.com>
2022-08-28 15:45:55 +0900

    볼트로 UI갱신

---
+++ Assets/DataTypes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[System.Serializable, Inspectable]
+public class TurnInfo
+{
+    [Inspectable]
+    public string name;
+    [Inspectable]
+    public string talk;
+    [Inspectable]
+    public Sprite image;
+
+    public string notInspectable;
+}
